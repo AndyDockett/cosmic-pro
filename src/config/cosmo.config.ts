631541573824cde93dd74c501335cec9a0aacc82
Andy <andy@Andys-MacBook-Pro.local>
2019-05-28 23:43:33 +0200

    test netlify
    test netlify

--- src/config/cosmo.config.ts
+++ src/config/cosmo.config.ts
@@ -3,7 +3,7 @@ export const config = {
     read_key: 'u54tfLMMYBd8jWpfrZE3z0p7pb7hPYrONn36Q9dqlv4zTEDDhx',
     write_key: 'gneFYsCTFXen5Jnp34OZ0tl7C6tyXJWikxgYGCfnV3aNhYyZ3J',
     bucket_id: '5ced99c009a4ce4d8646c61c',
-    bucket_slug: '0cdfea80-8187-11e9-ba63-0bb8fc570ad2',
+    bucket_slug: 'cosmic-proto',
     URL: 'https://api.cosmicjs.com/v1/',
     };
     
